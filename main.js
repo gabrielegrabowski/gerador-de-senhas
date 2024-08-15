34882f7738e3e80d2f50b2ad69c67eb6e17f3423
gabrielegrabowski <126891183+gabrielegrabowski@users.noreply.github.com>
2024-08-15 09:17:39 -0300

    finalizado

---
+++ main.js
@@ -0,0 +1,32 @@
+const numeroSenha =document.querySelector(parametro-Senha__texto);
+let tamanhoSenha.textContantent = tamanhoSenha;
+cont letraMaisculas =abcdefghijklmnopqrstuvwxyz;
+cont letMinuscula = abcdefghijklmnopqrstuvwxyz;
+
+const numeros = 0123456789;
+const sinbulos =#%&@*;
+const botoes  = document.querySelectorAll(.parametro-senha__botoes);
+const campoSenha =document.querySelectorAll(.#campo-senha);
+const checkbox = document.querySelectorAll(.checkbox);
+const forcaSenha = document.querySelector(forca);
+
+botoes
+{0}.onclinck = diminuir tamanhoSenha;
+function almentarTamanho;
+function diminuirTamanho(){
+
+    // tamanhoSenha = tamanhoSenha-1;
+    tamanhoSenha--;{
+ }
+
+ function almentarTamanho(){
+    if(tamanhoSenha< 20)}
+    // tamanhoSenha    = tamanhoSenha+1
+    tamanhoSenha++;
+    gerador 
+    }
+ }
+
+
+}
+
